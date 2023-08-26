2858460e0b8c14d28fd5209aff2a8e1704585e5e
tobi adihou <adihoutobi@gmail.com>
2023-08-26 19:56:06 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
+import { MenuComponent } from './composants/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageLoginComponent,
+    PageInscriptionComponent,
+    PageDashboardComponent,
+    PageStatistiquesComponent,
+    MenuComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
